b7b75cbe862165014f4533d588dd9af32b5d3227
LuisTorresDurante <luis.torres.durante@uabc.edu.mx>
2024-12-05 01:32:02 -0800

    Se agregaron el paco staff, el boleto ganador y el proyectil de casas

---
+++ Content/Items/Projectiles/BoletoGanador/Casa.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
+
+namespace CimaTerra.Content.Items.Projectiles.BoletoGanador
+{
+    internal class Casa : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.width = 16;
+            Projectile.height = 16;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.penetrate = 3;
+            Projectile.timeLeft = 600;
+        }
+
+        public override void AI()
+        {
+            Projectile.velocity.Y += Projectile.ai[0];
+            //if (Main.rand.NextBool(3))
+            //{
+            //    Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, ModContent.DustType<Sparkle>(), Projectile.velocity.X * 0.5f, Projectile.velocity.Y * 0.5f);
+            //}
+        }
+
+        public override bool OnTileCollide(Vector2 oldVelocity)
+        {
+            Projectile.penetrate--;
+            if (Projectile.penetrate <= 0)
+            {
+                Projectile.Kill();
+            }
+            else
+            {
+                Projectile.ai[0] += 0.1f;
+                if (Projectile.velocity.X != oldVelocity.X)
+                {
+                    Projectile.velocity.X = -oldVelocity.X;
+                }
+                if (Projectile.velocity.Y != oldVelocity.Y)
+                {
+                    Projectile.velocity.Y = -oldVelocity.Y;
+                }
+                Projectile.velocity *= 0.75f;
+                SoundEngine.PlaySound(SoundID.Item10, Projectile.position);
+            }
+            return false;
+        }
+
+        //public override void OnKill(int timeLeft)
+        //{
+        //    for (int k = 0; k < 5; k++)
+        //    {
+        //        Dust.NewDust(Projectile.position + Projectile.velocity, Projectile.width, Projectile.height, ModContent.DustType<Sparkle>(), Projectile.oldVelocity.X * 0.5f, Projectile.oldVelocity.Y * 0.5f);
+        //    }
+        //    SoundEngine.PlaySound(SoundID.Item25, Projectile.position);
+        //}
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            Projectile.ai[0] += 0.1f;
+            Projectile.velocity *= 0.75f;
+        }
+    }
+}
